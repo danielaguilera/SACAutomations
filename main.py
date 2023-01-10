2ffda97b5d7c694cb1b51a7d53033856b0d072e0
daguilera <daguilera@comscore.com>
2023-01-10 11:00:30 -0300

    changed driver

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import pyodbc
 
-original_driver = r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\User\Desktop\SAC\SAC Boletas.accdb"
-
 if __name__ == '__main__':
-    conn = pyodbc.connect(r"Driver={CData ODBC Driver for Microsoft Access};DBQ=C:\Users\User\Desktop\SAC\SAC Boletas.accdb")
+    conn = pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\daguilera\Desktop\SAC\SAC Boletas.accdb;")
     cursor = conn.cursor()
-    print(conn)
-    print(cursor)
-    print(cursor.tables())
-    for row in cursor.tables():
-        print(row.table_name)
+    # print(conn)
+    # print(cursor)
+
+    # for row in cursor.tables():
+    #     print(row.table_name)
     cursor.execute('select * from Tabla_nueva_de_boletas')
    
     for row in cursor.fetchall():
-       print (row)
-       break
-
-    msa_drivers = [x for x in pyodbc.drivers() if 'ACCESS' in x.upper()]
-
-    print(msa_drivers)
+        print (row)
+        break
