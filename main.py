c0ccb1e71d0771a69fcdaa02084c8b6a3501e371
danielaguilera <draguilera@uc.cl>
2023-01-10 09:55:10 -0300

    added drivers display

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import pyodbc
+
+original_driver = r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\User\Desktop\SAC\SAC Boletas.accdb"
+
 if __name__ == '__main__':
-    print('Code works')
+    conn = pyodbc.connect(r"Driver={CData ODBC Driver for Microsoft Access};DBQ=C:\Users\User\Desktop\SAC\SAC Boletas.accdb")
+    cursor = conn.cursor()
+    print(conn)
+    print(cursor)
+    print(cursor.tables())
+    for row in cursor.tables():
+        print(row.table_name)
+    cursor.execute('select * from Tabla_nueva_de_boletas')
+   
+    for row in cursor.fetchall():
+       print (row)
+       break
+
+    msa_drivers = [x for x in pyodbc.drivers() if 'ACCESS' in x.upper()]
+
+    print(msa_drivers)
