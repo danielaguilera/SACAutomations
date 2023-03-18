1d33da92f2721756ffc5fe3660c86f834092e3d9
danielaguilera <draguilera@uc.cl>
2023-03-18 17:22:41 -0300

    fixed sac ui problems with saving data

--- sac_ui.py
+++ sac_ui.py
@@ -6,12 +6,13 @@ from tkinter import filedialog
 from Utils.Metadata import *
 from Utils.GlobalFunctions import *
 from PyPDF2 import PdfReader, PdfMerger
+from PyPDF2.errors import PdfReadError
 import re
 
 class SACUI:
     def __init__(self, master: Tk):
         self.master = master
-        self.master.title("PDF App")
+        self.master.title("SAC App")
         
         self.uploadFrame = LabelFrame(master=self.master, text='Upload frame')
         self.uploadFrame.pack(expand=True, fill=BOTH)
@@ -19,8 +20,14 @@ class SACUI:
         self.boletaUploadButton = Button(self.uploadFrame, text="Subir boleta SII", width=20, height=1, font=('Helvetica bold', 26), command=self.selectBoletaPDF)
         self.boletaUploadButton.grid(row=0, column=0, sticky=NSEW, padx=5, pady=5)
         
+        self.boletaResetButton = Button(self.uploadFrame, text="Reestablecer boleta", width=20, height=1, font=('Helvetica bold', 15), command=self.resetBoleta)
+        self.boletaResetButton.grid(row=1, column=0, sticky=NSEW, padx=5, pady=5)
+        
         self.anexoUploadButton = Button(self.uploadFrame, text="Subir anexo", width=20, height=1, font=('Helvetica bold', 26), command=self.selectAnexoPDF)
         self.anexoUploadButton.grid(row=0, column=1, sticky=NSEW, padx=5, pady=5)
+
+        self.anexoResetButton = Button(self.uploadFrame, text="Reestablecer anexos", width=20, height=1, font=('Helvetica bold', 15), command=self.resetAnexos)
+        self.anexoResetButton.grid(row=1, column=1, sticky=NSEW, padx=5, pady=5)
         
         self.stateFrame = LabelFrame(master=self.master, text="State Frame")
         self.stateFrame.pack(expand=True, fill=BOTH)
@@ -31,11 +38,10 @@ class SACUI:
         self.numBoletaEntry = Entry(master=self.stateFrame, font=('Helvetica bold', 15))
         self.numBoletaEntry.grid(row=0, column=1, sticky=W, padx=5, pady=5)
         
-        self.uploadedBoletaLabel = Label(master=self.stateFrame, font=('Helvetica bold', 15), text='')
+        self.uploadedBoletaLabel = Label(master=self.stateFrame, font=('Helvetica bold', 10), text='No se ha subido boleta')
         self.uploadedBoletaLabel.grid(row=1, column=0, sticky=W, padx=5, pady=5)
-        self.uploadedBoletaLabel.grid_remove()
         
-        self.uploadedAnexosLabel = Label(master=self.stateFrame, font=('Helvetica bold', 15), text='')
+        self.uploadedAnexosLabel = Label(master=self.stateFrame, font=('Helvetica bold', 10), text='No se han subido anexos')
         self.uploadedAnexosLabel.grid(row=2, column=0, sticky=W, padx=5, pady=5)
         
         self.boletaPath: str = ''
@@ -46,6 +52,10 @@ class SACUI:
         
         self.saveButton = Button(self.saveFrame, text='Guardar', width=40, height=1, font=('Helvetica bold', 20), command=self.saveChanges)
         self.saveButton.pack(expand=True, fill=BOTH)
+        
+    @property
+    def numAnexos(self):
+        return len(self.anexosPaths)
                 
     def saveChanges(self):
         if not self.boletaPath:
@@ -61,53 +71,62 @@ class SACUI:
             os.makedirs(f'{DELIVEREDDATAPATH}/{numBoleta}')
         merger: PdfMerger = PdfMerger()
         for root in self.anexosPaths:
-            print(root)
             merger.append(root)
         merger.write(f'{DELIVEREDDATAPATH}/{numBoleta}/Anexo_{numBoleta}.pdf')
         merger.close()
         shutil.copy(self.boletaPath, f'{DELIVEREDDATAPATH}/{numBoleta}/Boleta_{numBoleta}.pdf')
         messagebox.showinfo(title='Mensaje', message=f'Archivos guardados para boleta n°{numBoleta}')
+        self.master.destroy()
 
     def selectBoletaPDF(self):
         filePath = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
-        if filePath:
+        if self.fileIsPDF(filePath):
+            self.boletaPath = filePath
+            self.uploadedBoletaLabel.config(text='Boleta subida')
             try:
-                reader = PdfReader(filePath)
-                index = reader.pages[0].extract_text().find('°')
-                if index == -1:
-                    index = reader.pages[0].extract_text().find('º')
-                print(index)
-                substring = reader.pages[0].extract_text()[index::]
-                end_index = substring.find('\n')
-                # numBoleta = re.search('°(.+?)\n', reader.pages[0].extract_text()).group(1).strip()
-                numBoleta = extractNumberFromText(reader.pages[0].extract_text()[index: end_index + 1])
-                print(numBoleta)
+                reader: PdfReader = PdfReader(filePath)
+                numberIndex: int = reader.pages[0].extract_text().find('°')
+                if numberIndex == -1:
+                    numberIndex = reader.pages[0].extract_text().find('º')
+                subString: str = reader.pages[0].extract_text()[numberIndex::]
+                endIndex: int = subString.find('\n')
+                subString = subString[0: endIndex + 1]
+                numBoleta = extractNumberFromText(subString)
                 if numBoleta.isdigit():
+                    self.numBoletaEntry.delete(0, END)
                     self.numBoletaEntry.insert(0, numBoleta)
                     messagebox.showinfo(title='Mensaje', message='Boleta subida correctamente')
                 else:
                     messagebox.showerror(title='Error', message='No se pudo encontrar el número de boleta en el documento. \nPor favor, ingréselo manualmente.')
-                self.boletaPath = filePath
-                self.uploadedBoletaLabel.config(text=f'Boleta SII subida: {self.boletaPath}')
-                self.uploadedBoletaLabel.grid()
             except Exception:
-                messagebox.showerror(title='Error', message='El archivo subido no es una boleta del SII')
+                messagebox.showerror(title='Error', message='No se pudo encontrar el número de boleta en el documento. \nPor favor, ingréselo manualmente.')
         else:
-            messagebox.showerror(title='Error', message='Archivo no encontrado')
+            messagebox.showerror(title='Error', message='Archivo no válido')
+            
+    def fileIsPDF(self, filePath: str):
+        try:
+            PdfReader(filePath)
+            return True
+        except Exception:
+            return False
+    
+    def resetBoleta(self):
+        self.boletaPath = ''
+        self.uploadedBoletaLabel.config(text='No se ha subido boleta')
+        self.numBoletaEntry.delete(0, END)
+        
+    def resetAnexos(self):
+        self.anexosPaths.clear()
+        self.uploadedAnexosLabel.config(text='No se han subido anexos')
                 
     def selectAnexoPDF(self):
         filePath = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
-        if filePath:
-            try:
-                reader = PdfReader(filePath)
-            except Exception:
-                messagebox.showerror(title='Error', message='El archivo no es de tipo PDF')
-            finally:
-                self.anexosPaths.append(filePath)
-                messagebox.showinfo(title='Mensaje', message='Anexo agregado correctamente')
-                self.uploadedAnexosLabel.config(text='Anexos subidos: \n' + '\n'.join(self.anexosPaths))
+        if self.fileIsPDF(filePath):
+            self.anexosPaths.append(filePath)
+            messagebox.showinfo(title='Mensaje', message='Anexo agregado correctamente')
+            self.uploadedAnexosLabel.config(text=f'Anexos subidos: {self.numAnexos}')
         else:
-            messagebox.showerror(title='Error', message='Archivo no encontrado')
+            messagebox.showerror(title='Error', message='Archivo no válido')
 
 root = Tk()
 pdf_app = SACUI(root)
