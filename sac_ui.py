358fdfbeafda747da8bc4732978ccadf1fdcfd3b
danielaguilera <draguilera@uc.cl>
2024-05-16 23:03:49 -0400

    se agregó usuario por defecto en sac

--- sac_ui.py
+++ sac_ui.py
@@ -3,5 +3,8 @@ from Utils.Metadata import *
 import sys
 
 if __name__ == '__main__':
-    user = sys.argv[1]
+    if (len(sys.argv) >= 2):
+        user = sys.argv[1]
+    else:
+        user = 'SERVIDOR'
     app = App(user=user)
