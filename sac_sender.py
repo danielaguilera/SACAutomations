a32da13d92b96dff74b980c2a04b66b65a120550
danielaguilera <draguilera@uc.cl>
2024-07-07 16:53:04 -0400

    se implementó eliminación de archivos caché al ejecutar sac sender

--- sac_sender.py
+++ sac_sender.py
@@ -9,29 +9,28 @@ import traceback
 
 if __name__ == '__main__':
     if getCacheFiles():
-        print('Aplicación en uso')
-    else:
-        with open('Params/mail_data.txt') as file:
-            username, password = file.readline().strip().split(',')
-        server = SMTPSERVERGYD
-        port = SMTPPORTGYD
-        try:
-            createCacheFile(user='SERVIDOR', script=SENDING_ACTIVITY)
-            sacSenderjob: SACSenderJob = SACSenderJob()
-            sacSenderjob.generateUnifiedDocument()
-            sacSenderjob.sendReports()
-            removeCacheFile(user='SERVIDOR', script=SENDING_ACTIVITY)
-        except Exception as e:
-            stringBuffer = io.StringIO()
-            traceback.print_exc(file=stringBuffer)
-            tracebackString = stringBuffer.getvalue()
-            stringBuffer.close()
-            with open('SAC_SENDER_ERROR.txt','w') as file:
-                file.write(tracebackString)
-            mailSender: MailSender = MailSender(senderUsername=username, senderPassword=password, smtpServer=server, smtpPort=port)
-            mailSender.sendMessage(receiverAddress='draguilera@uc.cl', mailSubject=f'[SAC SENDER] - [Error] - {datetime.now()}', mailContent=tracebackString)
-        finally:
-            removeCacheFile(user='SERVIDOR', script=SENDING_ACTIVITY)
+        removeAllCacheFiles()
+    with open('Params/mail_data.txt') as file:
+        username, password = file.readline().strip().split(',')
+    server = SMTPSERVERGYD
+    port = SMTPPORTGYD
+    try:
+        createCacheFile(user='SERVIDOR', script=SENDING_ACTIVITY)
+        sacSenderjob: SACSenderJob = SACSenderJob()
+        sacSenderjob.generateUnifiedDocument()
+        sacSenderjob.sendReports()
+        removeCacheFile(user='SERVIDOR', script=SENDING_ACTIVITY)
+    except Exception as e:
+        stringBuffer = io.StringIO()
+        traceback.print_exc(file=stringBuffer)
+        tracebackString = stringBuffer.getvalue()
+        stringBuffer.close()
+        with open('SAC_SENDER_ERROR.txt','w') as file:
+            file.write(tracebackString)
+        mailSender: MailSender = MailSender(senderUsername=username, senderPassword=password, smtpServer=server, smtpPort=port)
+        mailSender.sendMessage(receiverAddress='draguilera@uc.cl', mailSubject=f'[SAC SENDER] - [Error] - {datetime.now()}', mailContent=tracebackString)
+    finally:
+        removeCacheFile(user='SERVIDOR', script=SENDING_ACTIVITY)
     
     
         
